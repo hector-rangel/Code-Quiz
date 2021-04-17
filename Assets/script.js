715f9875e4497434d285638f92720e7ccf791c00
Hector Rangel <hector.rangeljr@yahoo.com>
2021-04-17 17:37:17 -0500

    added if statment for next question

--- Assets/script.js
+++ Assets/script.js
@@ -42,7 +42,7 @@ var quizQuestions = [{
 {
     question: "A very useful toll used during development and debugging for printing content to the debugger is:",
     choiceA: "JavaScript",
-    choiceB: "Bterminal/bash",
+    choiceB: "terminal/bash",
     choiceC: "for loops",
     choiceD: "console.log",
     correctAnswer: "D"
@@ -57,54 +57,62 @@ function startQuiz() {
         if (timeLeft > 1) {
             timerEl.textContent = 'Time left:' + timeLeft;
             timeLeft--;
-         } else {
+        } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
         }
     }, 1000);
-    display();
-  }
+    display(currentIndex);
+}
 
 var startPage = document.getElementById("startpage");
 var displayQuiz = document.getElementById("quiz");
 var question = document.getElementById("question");
-var currentQuiz = 0;
+var currentIndex = 0;
 
 
-  function display() {
-       // get rid or replace startpage text with question array
-       startPage.remove();
-       // remove startbtn
-       startBtn.remove();
-       // unhide quiz 
-       displayQuiz.classList.remove('hide');
-       // add questions to quiz
-       question.innerHTML = quizQuestions[0].question;
-       // add answers
-       A.innerHTML = quizQuestions[0].choiceA;
-       B.innerHTML = quizQuestions[0].choiceB;
-       C.innerHTML = quizQuestions[0].choiceC;
-       D.innerHTML = quizQuestions[0].choiceD;
-  }
-  // track user input
+function display(index) {
+    // get rid or replace startpage text with question array
+    startPage.remove();
+    // remove startbtn
+    startBtn.remove();
+    // unhide quiz 
+    displayQuiz.classList.remove('hide');
+    // add questions to quiz
+    question.innerHTML = quizQuestions[index].question;
+    // add answers
+    A.innerHTML = quizQuestions[index].choiceA;
+    B.innerHTML = quizQuestions[index].choiceB;
+    C.innerHTML = quizQuestions[index].choiceC;
+    D.innerHTML = quizQuestions[index].choiceD;
+}
+// track user input
 A.addEventListener('click', answers);
 B.addEventListener('click', answers);
 C.addEventListener('click', answers);
 D.addEventListener('click', answers);
 var results = document.getElementById("results");
-  function answers(e) {
- // right or wrong
- var userInput = e.target.id;
- if ( userInput === quizQuestions[0].correctAnswer){
-     results.innerHTML = 'correct';
- } else {
-     results.innerHTML = 'wrong';
-     timeLeft -= 10;
- }
-  // if wrong deduct 10secs
-  // go to next questions after checking
 
-  }
+function answers(e) {
+    // right or wrong
+    var userInput = e.target.id;
+    if (userInput === quizQuestions[currentIndex].correctAnswer) {
+        results.innerHTML = 'correct';
+    } else {
+        results.innerHTML = 'wrong';
+        // if wrong deduct 10 secs
+        timeLeft -= 10;
+    }
+    console.log(currentIndex);
+    // go to next question
+    currentIndex++;
+    if (timeLeft === 0 || currentIndex === quizQuestions.length) {
+        gameOver();
+    }
+    else {
+        display(currentIndex);
+    }
+}
 
 
 
